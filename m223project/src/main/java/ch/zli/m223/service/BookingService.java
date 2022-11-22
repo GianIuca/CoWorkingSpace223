be230551a13e1862a5cf8fd2d63a978e7a286b76
GianIuca <gianluca.ferrara@ti8m.ch>
2022-11-22 16:32:50 +0100

    Completed BookingService

--- m223project/src/main/java/ch/zli/m223/service/BookingService.java
+++ m223project/src/main/java/ch/zli/m223/service/BookingService.java
@@ -1,5 +1,50 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Booking;
+
+
 public class BookingService {
+
+@ApplicationScoped
+public class BuchungService {
+
+    @Inject
+    private EntityManager entityManager;
+
+    @Transactional
+    public Booking findBookingById(Long id) {
+        return  entityManager.find(Booking.class, id);
+    }
+
+    public List<Booking> getBookings() {
+        var query = entityManager.createQuery("FROM Buchung", Booking.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public Booking createBooking(Booking booking) {
+        entityManager.persist(booking);
+        return booking;
+    }
+
+    @Transactional
+    public void deleteBooking(Long id) {
+        var entity = entityManager.find(Booking.class, id);
+        entityManager.remove(entity);
+    }
     
+    @Transactional
+    public Booking updateBooking(Long id, Booking booking) {
+        return entityManager.merge(booking);
+    }
+
+
+}
 }
