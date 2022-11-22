4eb43afd3c62c01a0830585d5a7da72d6fb00139
GianIuca <gianluca.ferrara@ti8m.ch>
2022-11-22 22:52:20 +0100

    Added User HTTP Methodes

--- m223project/src/main/java/ch/zli/m223/controller/UserController.java
+++ m223project/src/main/java/ch/zli/m223/controller/UserController.java
@@ -1,5 +1,49 @@
 package ch.zli.m223.controller;
+import java.util.List;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.User;
+import ch.zli.m223.service.UserService;
+
+@Path("/user")
+@Tag(name = "Entries", description = "Handling of places")
 public class UserController {
     
+    @Inject
+    UserService userService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "returns all bookings", description = "returns all bookings")
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "create new bookin", description = "create new bookin")
+    public User create(User user) {
+        return userService.createUsers(user);
+    }
+
+   
+    @DELETE
+    @Path("/{id}")
+    @Operation(summary = "delete booking by ID", description = "delete booking by ID")
+    public void delete(@PathParam("id") Long id) {
+        userService.deleteUser(id);
+    }
 }
--- m223project/src/main/java/ch/zli/m223/service/UserService.java
+++ m223project/src/main/java/ch/zli/m223/service/UserService.java
@@ -1,5 +1,40 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.User;
+
+@ApplicationScoped
 public class UserService {
-    
+    //DELETE, POST PUT
+
+    @Inject
+    private EntityManager entityManager;
+
+    public List<User> getUsers() {
+        var qry = entityManager.createQuery("FROM User", User.class);
+        return qry.getResultList();
+    }
+
+    @Transactional
+    public User createUsers(User user) {
+        entityManager.merge(user);
+        return user;
+    }
+
+    @Transactional
+    public void deleteUser(Long id) {
+        var qry = entityManager.find(User.class, id);
+        entityManager.remove(qry);
+    }
+
+    @Transactional
+    public User updateUser(User user, Long id) {
+        return entityManager.merge(user);
+    }
 }
